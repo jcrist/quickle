2ee5acd08eaa6dacef4585ebc70c833f7f478229
Jim Crist-Harif <jcristharif@gmail.com>
2020-07-28 09:13:09 -0500

    Initial structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+ext_modules = [Extension("smolpickle._pickle", ["smolpickle/_pickle.c"])]
+
+setup(
+    name="smolpickle",
+    version="0.0.1",
+    maintainer="Jim Crist-Harif",
+    maintainer_email="jcristharif@gmail.com",
+    license="BSD",
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    long_description=(
+        open("README.rst").read() if os.path.exists("README.rst") else ""
+    ),
+    zip_safe=False,
+)
