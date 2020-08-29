93548f73e1e1ccad9727561f7c75bac9f1d13aac
Jim Crist-Harif <jcristharif@gmail.com>
2020-08-29 14:25:40 -0500

    Release version 0.0.3

--- quickle.c
+++ quickle.c
@@ -4,7 +4,7 @@
 PyDoc_STRVAR(quickle__doc__,
 "`quickle` - a quicker pickle.");
 
-#define QUICKLE_VERSION "0.0.2"
+#define QUICKLE_VERSION "0.0.3"
 
 enum opcode {
     MARK             = '(',
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ext_modules = [Extension("quickle", ["quickle.c"])]
 
 setup(
     name="quickle",
-    version="0.0.2",
+    version="0.0.3",
     maintainer="Jim Crist-Harif",
     maintainer_email="jcristharif@gmail.com",
     url="https://github.com/jcrist/quickle",
