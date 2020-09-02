7c847b3970dc1e9afc44a7b8158784dca51d1522
Jim Crist-Harif <jcristharif@gmail.com>
2020-09-02 17:04:26 -0500

    :black_circle:

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
 default_role = "obj"
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 templates_path = ["_templates"]
 html_static_path = ["_static"]
